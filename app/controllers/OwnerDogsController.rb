60d39c57874297269f9e88ff2a0d62ccdcefb8ca
github username <willmharris0@gmail>
2020-02-20 17:53:39 -0500

    We tried.... will edit 3

--- app/controllers/OwnerDogsController.rb
+++ app/controllers/OwnerDogsController.rb
@@ -1,3 +1,6 @@
 class OwnerDogsController < Sinatra::Base
-
+    # post "/join" do 
+    #     dog = Dog.find_by(name: params[:added_dog])
+    #     OwnerDog.create(owner_id: ?, dog_id: dog.id)
+    # end 
 end
